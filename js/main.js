43deb9980195d7e97c340cce7a696057b21126fe
T-a-g-z <tarunsingh7m7@gmail.com>
2018-08-25 14:40:21 +0530

    Version 2

--- js/main.js
+++ js/main.js
@@ -34,3 +34,4 @@ sr.reveal('.footer', {
     viewFactor:0.2
 });
 });
+
---
+++ js/texteditor.js
@@ -0,0 +1,24 @@
+function run(){
+    var text = document.getElementById("textareaCode").value;
+
+    var ifr = document.createElement('iframe');
+
+    ifr.setAttribute("frameborder", "0");
+    ifr.setAttribute("id", "iframeResult");
+    ifr.setAttribute("name", "iframeResult");
+
+    document.getElementById('iframewrapper').innerHTML="";
+
+    document.getElementById("iframewrapper").appendChild(ifr);
+
+    var ifrw = (ifr.contentWindow) ? ifr.contentWindow : (ifr.contentDocument.document) ? ifr.contentDocument.document : ifr.contentDocument;
+    ifrw.document.open();
+
+    ifrw.document.write(text);
+    ifrw.document.close();
+    if (ifrw.document.body && ifrw.document.body.isContentEditable){
+        ifrw.document.body.contentEditable = true;
+
+        ifrw.document.body.contentEditable = false;
+    }
+}
